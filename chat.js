d842467ebb0aeec15db902e915ae77a9269dfb68
lcomino <lcomino3d@gmail.com>
2015-08-03 22:59:16 -0300

    Criando persistencia do usuários.. ainda com bugs :(

--- chat.js
+++ chat.js
@@ -1,10 +1,19 @@
 "use strict";
 
 var io = require("socket.io")(5430);
-
+var users = [];
 io.on('connection', function(socket){
-  socket.broadcast.emit('new user', socket.id);
+
   console.log('Usuário entrou..', socket.id);
+  socket.on('create', function(subject, userName){
+    var user = {};
+    user.id = socket.id;
+    user.name = userName;
+    users.push(user);
+    //socket.broadcast.emit('new user', user);
+    console.log(user.name.toString());
+    socket.emit('new user', user);
+  });
   socket.on('message', function(message){
     console.log(message);
     io.emit('message', message, socket.id);
